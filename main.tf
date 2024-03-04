c68aa82c009c2aae1cf3ce9a8e1206e4814173a7
Rajesh chandran <146159381+rajeshchandranaws1@users.noreply.github.com>
2024-03-04 19:29:02 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ resource "aws_launch_template" "template" {
   name_prefix     = "test"
   image_id        = "ami-0440d3b780d96b29d"
   instance_type   = "t2.micro"
-  key_name        = "kp-mar4.pem"
+  key_name        = "kp-mar4"
 }
 
 resource "aws_autoscaling_group" "autoscale" {
