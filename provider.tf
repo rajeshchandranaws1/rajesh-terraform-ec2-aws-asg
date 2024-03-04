6acc3e10ff04a067ed0efb632c0932eb888e4a1b
Rajesh chandran <146159381+rajeshchandranaws1@users.noreply.github.com>
2024-03-04 18:09:20 +0530

    Create provider.tf

---
+++ provider.tf
@@ -0,0 +1,12 @@
+#STEP1: DEFINE AWS VERSION
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 5.0.0"
+    }
+  }
+}
+#STEP2: DEFINE THE REGION (N. Virginia)
+provider "aws" {
+  region = "us-east-1"
